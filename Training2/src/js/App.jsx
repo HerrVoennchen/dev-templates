3324997369b94d2c8de93d4b2fb7d92b85ec50f5
Unknown <sebastian.kruemling@gmail.com>
2016-10-06 22:42:22 +0200

    more middleware

--- Training2/src/js/App.jsx
+++ Training2/src/js/App.jsx
@@ -3,6 +3,7 @@ import { combineReducers, applyMiddleware, createStore } from 'redux';
 import axios from 'axios';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 
 const initialState = {
 	fetching: false,
@@ -13,15 +14,15 @@ const initialState = {
 
 const reducer = (state={}, action) => {
 	switch(action.type) {
-		case 'FETCH_USERS_START': {
+		case 'FETCH_USERS_PENDING': {
 			return {...state, fetching: true};
 			break;
 		}
-		case 'RECEIVE_USERS': {
+		case 'FETCH_USERS_FULFILLED': {
 			return {...state, fetching: false, fetched: true, users: action.payload};
 			break;
 		}
-		case 'FETCH_USERS_ERROR': {
+		case 'FETCH_USERS_REJECTED': {
 			return {...state, fetching: false, fetched: false, err: action.payload};
 			break;
 		}
@@ -31,9 +32,10 @@ const reducer = (state={}, action) => {
 }
 
 
-const middleware = applyMiddleware(thunk, logger());
+const middleware = applyMiddleware(thunk, logger(), promise());
 const store = createStore(reducer, middleware);
 
+/*
 store.dispatch((dispatch) => {
 	dispatch({type: 'FETCH_USERS_START'});
 	axios.get('http://rest.learncode.academy/api/wstern/users')
@@ -44,4 +46,10 @@ store.dispatch((dispatch) => {
 		.catch((err) => {
 			dispatch({type: 'FETCH_USERS_ERROR', payload: err});
 		});
+});
+*/
+
+store.dispatch({
+	type: 'FETCH_USERS',
+	payload: axios.get('http://rest.learncode.academy/api/wstern/users')
 });
