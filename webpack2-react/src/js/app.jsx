96f9a2303593e81d8857131748a5ac620b967015
Unknown <sebastian.kruemling@gmail.com>
2017-02-03 23:10:28 +0100

    mit bootstrap

--- webpack2-react/src/js/app.jsx
+++ webpack2-react/src/js/app.jsx
@@ -1,9 +1,16 @@
 import '../css/app.css';
+import 'jquery';
+import 'tether';
+import 'bootstrap';
 
 import React from 'react';
 import { render } from 'react-dom';
 
 render(
-	<h1>Hello, World! 2</h1>,
+	<div>
+		<h1>Hello, World! 2</h1>
+		<br/>
+		<button type="button" className="btn btn-secondary">Primary</button>
+	</div>,
 	document.getElementById('App')
 	);
--- webpack2-react/webpack.config.js
+++ webpack2-react/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
 		}]
 	},
 	resolve: {
-		modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+		modules: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')],
 		extensions: ['.js', '.jsx', '.css', '.html']
 	},
 	plugins: [
@@ -39,6 +39,24 @@ module.exports = {
 		filename: 'app.bundle.css',
 		allChunks: true
 	}),
+	new webpack.ProvidePlugin({
+		jQuery: 'jquery',
+		$: 'jquery',
+		jquery: 'jquery',
+		Tether: "tether",
+		"window.Tether": "tether",
+		Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+		Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+		Button: "exports-loader?Button!bootstrap/js/dist/button",
+		Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+		Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+		Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+		Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+		Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+		Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+		Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+		Util: "exports-loader?Util!bootstrap/js/dist/util"
+	})
 	/*new HtmlWebpackPlugin({
 		template: path.join(__dirname, 'src/index.html'),
 		filename: 'index.html',
